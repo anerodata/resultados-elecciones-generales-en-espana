4903c84e963b905f6521b5b9c96f4508f70d1743
anerodata <antonio92.ahr@gmail.com>
2023-11-18 13:05:53 +0100

    Create class for creating the builder

---
+++ dashboard/src/js/votesProvincesTable/components/ProvinceDataBuilder.js
@@ -0,0 +1,71 @@
+import { votesPerProvinceBefore, votesPerProvinceNow } from './../../mockup-data.js'
+function getProvinceVotesBefore (codigo) {
+  return votesPerProvinceBefore.filter(d => d['Código de Provincia'] === codigo)
+}
+function getProvinceVotesNow (codigo) {
+  return votesPerProvinceNow.filter(d => d['Código de Provincia'] === codigo)
+}
+class ProvinceDataBuilder {
+  constructor (provinceCode) {
+    this.provinceCode = provinceCode
+    this.multiple = 100
+  }
+
+  setup () {
+    const provinceVotesBefore = getProvinceVotesBefore(this.provinceCode)
+    const provinceVotesNow = getProvinceVotesNow(this.provinceCode)
+    console.log(provinceVotesBefore, provinceVotesNow)
+    return [
+      {
+        nombre: 'Unidas Podemos',
+        color: 'red',
+        // Entre 100
+        votesNum: 1100000 / this.multiple,
+        votesPreviousNum: 30000 / this.multiple,
+        par_meta_id: 37,
+        dif: 20.480999999999998
+      },
+      {
+        nombre: 'PP',
+        color: '#C6A15B',
+        votesNum: 100000 / this.multiple,
+        votesPreviousNum: 240304 / this.multiple,
+        par_meta_id: 38,
+        dif: 9.972000000000005
+      },
+      {
+        nombre: 'PSOE',
+        color: '#C6A15B',
+        votesNum: 26381 / this.multiple,
+        votesPreviousNum: 39595 / this.multiple,
+        par_meta_id: 40,
+        dif: -13.213999999999999
+      },
+      {
+        nombre: 'EH Bildu',
+        color: '#C6A15B',
+        votesNum: 15858 / this.multiple,
+        votesPreviousNum: 24687 / this.multiple,
+        par_meta_id: 15,
+        dif: -8.829
+      },
+      {
+        nombre: 'Cs',
+        color: '#C6A15B',
+        votesNum: 8372 / this.multiple,
+        votesPreviousNum: 7039 / this.multiple,
+        par_meta_id: 10,
+        dif: 1.3330000000000002
+      },
+      {
+        nombre: 'Vox',
+        color: '#C6A15B',
+        votesNum: 0.306 / this.multiple,
+        votesPreviousNum: 5.587 / this.multiple,
+        par_meta_id: 52,
+        dif: -5.281
+      }
+    ]
+  }
+}
+export default ProvinceDataBuilder
--- dashboard/src/js/votesProvincesTable/votesProvincesTable.js
+++ dashboard/src/js/votesProvincesTable/votesProvincesTable.js
@@ -2,8 +2,8 @@ import { provinces, colors } from './utils/constants.js'
 import provinceTable from './provinceTable/provinceTable.js'
 import { votesPerProvinceBefore, votesPerProvinceNow } from './../mockup-data.js'
 import ProvinceSelect from './components/ProvincesSelect.js'
+import ProvinceDataBuilder from './components/ProvinceDataBuilder.js'
 function votesProvincesTable (idDivMain, idTable, multiple) {
-  const select = document.getElementById('select')
   const provinceSelect = new ProvinceSelect('select')
   provinceSelect.setup()
   init()
@@ -11,74 +11,19 @@ function votesProvincesTable (idDivMain, idTable, multiple) {
     init()
   }
   function init () {
-    const dataset = getData(select.value, votesPerProvinceBefore, votesPerProvinceNow)
+    const provinceDataBuilder = new ProvinceDataBuilder(provinces[0].code)
+    const dataset = provinceDataBuilder.setup()
     provinceTable(provinces[0].name, dataset, idDivMain, idTable)
     setEventSelect()
   }
   function setEventSelect () {
     document.getElementById('select').onchange = function () {
       const selectedProvince = provinces.find(d => d.code === this.value)
-      const dataset = getData(selectedProvince.code, votesPerProvinceBefore, votesPerProvinceNow)// REMOTO
+      const provinceDataBuilder = new ProvinceDataBuilder(selectedProvince.code)
+      const dataset = provinceDataBuilder.setup()
+      console.log(dataset)
       provinceTable(selectedProvince.name, dataset, idDivMain, idTable)
     }
-  }
-
-  function getData (codigo, votesPerProvinceBefore, votesPerProvinceNow) {
-    const provinceVotesBefore = votesPerProvinceBefore.filter(d => d['Código de Provincia'] === codigo)
-    const provinceVotesNow = votesPerProvinceNow.filter(d => d['Código de Provincia'] === codigo)
-    console.log(provinceVotesBefore, provinceVotesNow)
-    const dataset = [
-      {
-        nombre: 'Unidas Podemos',
-        color: 'red',
-        // Entre 100
-        votesNum: 1100000 / multiple,
-        votesPreviousNum: 30000 / multiple,
-        par_meta_id: 37,
-        dif: 20.480999999999998
-      },
-      {
-        nombre: 'PP',
-        color: '#C6A15B',
-        votesNum: 100000 / multiple,
-        votesPreviousNum: 240304 / multiple,
-        par_meta_id: 38,
-        dif: 9.972000000000005
-      },
-      {
-        nombre: 'PSOE',
-        color: '#C6A15B',
-        votesNum: 26381 / multiple,
-        votesPreviousNum: 39595 / multiple,
-        par_meta_id: 40,
-        dif: -13.213999999999999
-      },
-      {
-        nombre: 'EH Bildu',
-        color: '#C6A15B',
-        votesNum: 15858 / multiple,
-        votesPreviousNum: 24687 / multiple,
-        par_meta_id: 15,
-        dif: -8.829
-      },
-      {
-        nombre: 'Cs',
-        color: '#C6A15B',
-        votesNum: 8372 / multiple,
-        votesPreviousNum: 7039 / multiple,
-        par_meta_id: 10,
-        dif: 1.3330000000000002
-      },
-      {
-        nombre: 'Vox',
-        color: '#C6A15B',
-        votesNum: 0.306 / multiple,
-        votesPreviousNum: 5.587 / multiple,
-        par_meta_id: 52,
-        dif: -5.281
-      }
-    ]
-    return dataset
   }
 }
 export default votesProvincesTable
