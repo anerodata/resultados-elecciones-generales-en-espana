cf1a5f916bd6bde29a1b1aba9d405c242b091e50
anerodata <antonio92.ahr@gmail.com>
2023-11-18 17:20:46 +0100

    Refactor: app.js

--- dashboard/src/js/app.js
+++ dashboard/src/js/app.js
@@ -3,29 +3,35 @@ import ProvinceSelect from './components/ProvincesSelect.js'
 import ProvinceDataBuilder from './components/ProvinceDataBuilder.js'
 import provinceTable from './components/ProvinceVisTable.js'
 
+const idDivMain = 'main'
+const idTable = 'provinces-table'
+const provinceSelect = new ProvinceSelect('select')
+
 function app () {
-  const idDivMain = 'main'
-  const idTable = 'provinces-table'
-  const provinceSelect = new ProvinceSelect('select')
-  provinceSelect.setup()
-  init()
   window.onresize = function () {
     init()
   }
-  function init () {
-    const provinceDataBuilder = new ProvinceDataBuilder(provinces[0].code)
-    const provinceDataset = provinceDataBuilder.setup()
-    provinceTable(provinces[0].name, provinceDataset, idDivMain, idTable)
-    setEventSelect()
-  }
-  function setEventSelect () {
-    document.getElementById('select').onchange = function () {
-      const selectedProvince = provinces.find(d => d.code === this.value)
-      const provinceDataBuilder = new ProvinceDataBuilder(selectedProvince.code)
-      const provinceDataset = provinceDataBuilder.setup()
-      console.log(provinceDataset)
-      provinceTable(selectedProvince.name, provinceDataset, idDivMain, idTable)
-    }
+  init()
+}
+
+function init () {
+  provinceSelect.setup()
+  setupProvinceTable(provinces[0].code)
+  setEventSelect()
+}
+
+function setEventSelect () {
+  document.getElementById('select').onchange = function () {
+    const selectedProvince = provinces.find(d => d.code === this.value)
+    setupProvinceTable(selectedProvince.code)
   }
 }
+
+function setupProvinceTable (provinceCode) {
+  console.log(provinceCode)
+  const provinceDataBuilder = new ProvinceDataBuilder(provinceCode)
+  const provinceDataset = provinceDataBuilder.setup()
+  provinceTable(provinces[0].name, provinceDataset, idDivMain, idTable)
+}
+
 export default app
