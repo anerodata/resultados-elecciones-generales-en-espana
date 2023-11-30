6ab416dbdf14106c3deb1764fd3b9fc05c22a39c
anerodata <antonio92.ahr@gmail.com>
2023-11-30 19:03:09 +0100

    Include diff symbol in second table

--- dashboard/src/js/components/ProvinceVisTdVariation.js
+++ dashboard/src/js/components/ProvinceVisTdVariation.js
@@ -1,13 +1,36 @@
+import up from '../../assets/img/up.png'
+import down from '../../assets/img/down.png'
+import equal from '../../assets/img/equal.png'
+function getDiffSymbolSrc (diff) {
+  if (diff > 0) {
+    return up
+  } else if (diff < 0) {
+    return down
+  } else {
+    return equal
+  }
+}
+function getDiffSymbolColor (diff) {
+  if (diff > 0) {
+    return '#4DFFC7'
+  } else if (diff < 0) {
+    return '#FF4D7A'
+  } else {
+    return 'black'
+  }
+}
 class ProvinceVisTdVariation {
   constructor (config) {
     this.value = config.value
   }
 
   getTdNode () {
-    const span = document.createElement('span')
-    const textNode = document.createTextNode(this.value)
-    span.appendChild(textNode)
-    return span
+    const imgNode = document.createElement('img')
+    imgNode.src = getDiffSymbolSrc(this.value)
+    imgNode.classList.add('imgVar')
+    imgNode.setAttribute('data-num', this.value)
+    imgNode.setAttribute('data-color', getDiffSymbolColor(this.value))
+    return imgNode
   }
 }
 export default ProvinceVisTdVariation
