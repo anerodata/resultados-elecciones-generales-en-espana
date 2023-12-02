6ed1b535873fa48544e2beb813b6c9c1d9205c0f
anerodata <antonio92.ahr@gmail.com>
2023-12-02 11:29:53 +0100

    Draw canvas visualization

--- dashboard/src/js/components/ProvinceVisTdDotChart.js
+++ dashboard/src/js/components/ProvinceVisTdDotChart.js
@@ -1,17 +1,97 @@
 import * as d3 from 'd3'
+let height = 0
+const setupCustomBase = new WeakMap()
+const setupCanvas = new WeakMap()
+
 class ProvinceVisTdDotChart {
   constructor (config) {
     this.value = config.value
     this.color = config.color
-    this.chartDimensions = config.chartDimensions
+    this.width = config.chartDimensions.chartWidth
+    this.widthSq = config.chartDimensions.widthSq
+    this.sep = config.chartDimensions.sep
+    setupCustomBase.set(this, () => {
+      let customBase = document.createElement('custom')
+      customBase = d3.select(customBase)
+      let iX = 0
+      let iY = 0
+      let bStart = true
+
+      let nBlockX = 0
+      let nBlockY = 0
+
+      let countTen = 0
+      let countHund = 0
+
+      for (let i = 0; i < this.value; i++) {
+        customBase.append('custom')
+          .attr('class', 'square')
+          .attr('width', this.widthSq)
+          .attr('height', this.widthSq)
+          .attr('x', () => {
+            if (!bStart) {
+              countTen += 1
+              countHund += 1
+              // nuevo renglon dentro del cuadro
+              if (countTen === 10 && countHund < 100) {
+                countTen = 0
+                iX = nBlockX
+                iY += this.sep + 1
+              } else if (countHund === 100) {
+                countHund = 0
+                countTen = 0
+                // nuevo renglon en otro cuadro debajo
+                if (nBlockX + ((this.sep * 12) + 10) * 2 > this.width) {
+                  nBlockX = 0
+                  iX = 0
+                  nBlockY += this.sep * 12 + 10
+                  iY = nBlockY
+                  /// /nuevo renglon en otro cuadro a continuacion
+                } else {
+                  nBlockX += this.sep * 12 + 10
+                  iX = nBlockX
+                  iY = nBlockY
+                }
+              } else {
+                // nuevo punto a continuacion del anterior
+                iX += this.sep + 1
+              }
+            }
+            // comienzo
+            bStart = false
+            return iX
+          })
+          .attr('y', function () {
+            return iY
+          })
+          .attr('fillStyle', this.color)
+      }
+      height = nBlockY + this.widthSq + this.sep * 12 + 10 + 3
+      return customBase
+    })
+    setupCanvas.set(this, (customBase) => {
+      let canvas = document.createElement('canvas')
+      canvas = d3.select(canvas)
+        .attr('width', this.width)
+        .attr('height', height)
+
+      const context = canvas.node().getContext('2d')
+      context.clearRect(0, 0, this.width, height)
+      const elements = customBase.selectAll('custom.square')
+      elements.each(function () {
+        const node = d3.select(this)
+        context.fillStyle = node.attr('fillStyle')
+        console.log(node.attr('x'), node.attr('y'), node.attr('width'), node.attr('height'))
+        context.fillRect(node.attr('x'), node.attr('y'), node.attr('width'), node.attr('height'))
+      })
+      return canvas.node()
+    })
   }
 
   getTdNode () {
-    console.log(this)
-    const span = document.createElement('span')
-    const textNode = document.createTextNode(this.value)
-    span.appendChild(textNode)
-    return span
+    const customBase = setupCustomBase.get(this)()
+    const canvas = setupCanvas.get(this)(customBase)
+    return canvas
   }
 }
 export default ProvinceVisTdDotChart
