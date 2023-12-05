c6ff1d715ec298a3b2e6d8501dbece50d896d746
anerodata <antonio92.ahr@gmail.com>
2023-12-05 20:46:03 +0100

    Implement logic in class

--- dashboard/src/js/components/ProvinceVisTdDotChart.js
+++ dashboard/src/js/components/ProvinceVisTdDotChart.js
@@ -2,6 +2,7 @@ import * as d3 from 'd3'
 let height = 0
 const setupCustomBase = new WeakMap()
 const setupVis = new WeakMap()
+const calcCoordinates = new WeakMap()
 
 class ProvinceVisTdDotChart {
   constructor (config) {
@@ -11,8 +12,6 @@ class ProvinceVisTdDotChart {
     this.widthSq = config.chartDimensions.widthSq
     this.sep = config.chartDimensions.sep
     setupCustomBase.set(this, () => {
-      let customBase = document.createElement('custom')
-      customBase = d3.select(customBase)
       let iX = 0
       let iY = 0
       let bStart = true
@@ -22,52 +21,57 @@ class ProvinceVisTdDotChart {
 
       let countTen = 0
       let countHund = 0
-
-      for (let i = 0; i < this.value; i++) {
-        customBase.append('custom')
-          .attr('class', 'square')
-          .attr('width', this.widthSq)
-          .attr('height', this.widthSq)
-          .attr('x', () => {
-            if (!bStart) {
-              countTen += 1
-              countHund += 1
-              // nuevo renglon dentro del cuadro
-              if (countTen === 10 && countHund < 100) {
-                countTen = 0
-                iX = nBlockX
-                iY += this.sep + 1
-              } else if (countHund === 100) {
-                countHund = 0
-                countTen = 0
-                // nuevo renglon en otro cuadro debajo
-                if (nBlockX + ((this.sep * 12) + 10) * 2 > this.width) {
-                  nBlockX = 0
-                  iX = 0
-                  nBlockY += this.sep * 12 + 10
-                  iY = nBlockY
-                  /// /nuevo renglon en otro cuadro a continuacion
-                } else {
-                  nBlockX += this.sep * 12 + 10
-                  iX = nBlockX
-                  iY = nBlockY
-                }
-              } else {
-                // nuevo punto a continuacion del anterior
-                iX += this.sep + 1
-              }
+      const customBase = document.createElement('custom')
+      let coordinates = {}
+      const calcCoordinates = () => {
+        if (!bStart) {
+          countTen += 1
+          countHund += 1
+          // nuevo renglon dentro del cuadro
+          if (countTen === 10 && countHund < 100) {
+            countTen = 0
+            iX = nBlockX
+            iY += this.sep + 1
+          } else if (countHund === 100) {
+            countHund = 0
+            countTen = 0
+            // nuevo renglon en otro cuadro debajo
+            if (nBlockX + ((this.sep * 12) + 10) * 2 > this.width) {
+              nBlockX = 0
+              iX = 0
+              nBlockY += this.sep * 12 + 10
+              iY = nBlockY
+              /// /nuevo renglon en otro cuadro a continuacion
+            } else {
+              nBlockX += this.sep * 12 + 10
+              iX = nBlockX
+              iY = nBlockY
             }
-            // comienzo
-            bStart = false
-            return iX
-          })
-          .attr('y', function () {
-            return iY
-          })
-          .attr('fillStyle', this.color)
+          } else {
+            // nuevo punto a continuacion del anterior
+            iX += this.sep + 1
+          }
+        }
+        // comienzo
+        bStart = false
+        return { x: iX, y: iY, nBlockY }
       }
-      height = nBlockY + this.widthSq + this.sep * 12 + 10 + 3
+      for (let i = 0; i < this.value; i++) {
+        const customSquare = document.createElement('custom')
+        customBase.appendChild(customSquare)
+        customSquare.classList.add('square')
+        customSquare.setAttribute('width', `${this.widthSq}`) // .width = `${this.widthSq}px`
+        customSquare.setAttribute('height', `${this.widthSq}`) // .height = `${this.widthSq}px`
+        coordinates = calcCoordinates()
+        customSquare.setAttribute('x', coordinates.x)
+        customSquare.setAttribute('y', coordinates.y)
+        customSquare.setAttribute('fillStyle', this.color)
+      }
+      height = coordinates.nBlockY + this.widthSq + this.sep * 12 + 10 + 3
+      console.log(customBase.children)
       return customBase
+    })
+    calcCoordinates.set(this, () => {
     })
     setupVis.set(this, (customBase) => {
       let canvas = document.createElement('canvas')
@@ -85,7 +89,6 @@ class ProvinceVisTdDotChart {
       })
       const canvasContainer = document.createElement('div')
       canvasContainer.appendChild(canvas.node())
-      console.log(height)
       canvasContainer.style.height = `${height}px`
       return canvasContainer
     })
@@ -93,7 +96,7 @@ class ProvinceVisTdDotChart {
 
   getTdNode () {
     const customBase = setupCustomBase.get(this)()
-    const vis = setupVis.get(this)(customBase)
+    const vis = setupVis.get(this)(d3.select(customBase))
     return vis
   }
 }
