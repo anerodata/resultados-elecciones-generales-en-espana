2ab589682f9ec4cbf55918268c194fae3e84f80b
anerodata <antonio92.ahr@gmail.com>
2023-12-06 13:29:58 +0100

    Round value, use calcFactors

--- dashboard/src/js/components/ProvinceDataBuilder.js
+++ dashboard/src/js/components/ProvinceDataBuilder.js
@@ -23,48 +23,48 @@ class ProvinceDataBuilder {
         nombre: 'Unidas Podemos',
         color: 'red',
         // Entre 100
-        votesNum: 1100000 / this.multiple,
-        votesPreviousNum: 30000 / this.multiple,
+        votesNum: Math.round(1100000 / this.multiple),
+        votesPreviousNum: Math.round(30000 / this.multiple),
         par_meta_id: 37,
         dif: 20.480999999999998
       },
       {
         nombre: 'PP',
         color: '#C6A15B',
-        votesNum: 100000 / this.multiple,
-        votesPreviousNum: 240304 / this.multiple,
+        votesNum: Math.round(100000 / this.multiple),
+        votesPreviousNum: Math.round(240304 / this.multiple),
         par_meta_id: 38,
         dif: 9.972000000000005
       },
       {
         nombre: 'PSOE',
         color: '#C6A15B',
-        votesNum: 26381 / this.multiple,
-        votesPreviousNum: 39595 / this.multiple,
+        votesNum: Math.round(26381 / this.multiple),
+        votesPreviousNum: Math.round(39595 / this.multiple),
         par_meta_id: 40,
         dif: -13.213999999999999
       },
       {
         nombre: 'EH Bildu',
         color: '#C6A15B',
-        votesNum: 15858 / this.multiple,
-        votesPreviousNum: 24687 / this.multiple,
+        votesNum: Math.round(15858 / this.multiple),
+        votesPreviousNum: Math.round(24687 / this.multiple),
         par_meta_id: 15,
         dif: -8.829
       },
       {
         nombre: 'Cs',
         color: '#C6A15B',
-        votesNum: 8372 / this.multiple,
-        votesPreviousNum: 7039 / this.multiple,
+        votesNum: Math.round(8372 / this.multiple),
+        votesPreviousNum: Math.round(7039 / this.multiple),
         par_meta_id: 10,
         dif: 1.3330000000000002
       },
       {
         nombre: 'Vox',
         color: '#C6A15B',
-        votesNum: 0.306 / this.multiple,
-        votesPreviousNum: 5.587 / this.multiple,
+        votesNum: Math.round(0.306 / this.multiple),
+        votesPreviousNum: Math.round(5.587 / this.multiple),
         par_meta_id: 52,
         dif: -5.281
       }
--- dashboard/src/js/components/ProvinceVisTdDotChart.js
+++ dashboard/src/js/components/ProvinceVisTdDotChart.js
@@ -3,6 +3,15 @@ let height = 0
 const setupCustomBase = new WeakMap()
 const setupVis = new WeakMap()
 const calcCoordinates = new WeakMap()
+function CalcFactors () {
+  this.iX = 0
+  this.iY = 0
+  this.bStart = true
+  this.nBlockX = 0
+  this.nBlockY = 0
+  this.countTen = 0
+  this.countHund = 0
+}
 
 class ProvinceVisTdDotChart {
   constructor (config) {
@@ -12,49 +21,41 @@ class ProvinceVisTdDotChart {
     this.widthSq = config.chartDimensions.widthSq
     this.sep = config.chartDimensions.sep
     setupCustomBase.set(this, () => {
-      let iX = 0
-      let iY = 0
-      let bStart = true
-
-      let nBlockX = 0
-      let nBlockY = 0
-
-      let countTen = 0
-      let countHund = 0
+      const calcFactors = new CalcFactors()
       const customBase = document.createElement('custom')
       let coordinates = {}
-      const calcCoordinates = () => {
-        if (!bStart) {
-          countTen += 1
-          countHund += 1
+      const calcCoordinates = (calcFactors, i, value) => {
+        if (!calcFactors.bStart) {
+          calcFactors.countTen += 1
+          calcFactors.countHund += 1
           // nuevo renglon dentro del cuadro
-          if (countTen === 10 && countHund < 100) {
-            countTen = 0
-            iX = nBlockX
-            iY += this.sep + 1
-          } else if (countHund === 100) {
-            countHund = 0
-            countTen = 0
+          if (calcFactors.countTen === 10 && calcFactors.countHund < 100) {
+            calcFactors.countTen = 0
+            calcFactors.iX = calcFactors.nBlockX
+            calcFactors.iY += this.sep + 1
+          } else if (calcFactors.countHund === 100) {
+            calcFactors.countHund = 0
+            calcFactors.countTen = 0
             // nuevo renglon en otro cuadro debajo
-            if (nBlockX + ((this.sep * 12) + 10) * 2 > this.width) {
-              nBlockX = 0
-              iX = 0
-              nBlockY += this.sep * 12 + 10
-              iY = nBlockY
+            if (calcFactors.nBlockX + ((this.sep * 12) + 10) * 2 > this.width) {
+              calcFactors.nBlockX = 0
+              calcFactors.iX = 0
+              calcFactors.nBlockY += this.sep * 12 + 10
+              calcFactors.iY = calcFactors.nBlockY
               /// /nuevo renglon en otro cuadro a continuacion
             } else {
-              nBlockX += this.sep * 12 + 10
-              iX = nBlockX
-              iY = nBlockY
+              calcFactors.nBlockX += this.sep * 12 + 10
+              calcFactors.iX = calcFactors.nBlockX
+              calcFactors.iY = calcFactors.nBlockY
             }
           } else {
             // nuevo punto a continuacion del anterior
-            iX += this.sep + 1
+            calcFactors.iX += this.sep + 1
           }
         }
         // comienzo
-        bStart = false
-        return { x: iX, y: iY, nBlockY }
+        calcFactors.bStart = false
+        return { x: calcFactors.iX, y: calcFactors.iY, nBlockY: calcFactors.nBlockY }
       }
       for (let i = 0; i < this.value; i++) {
         const customSquare = document.createElement('custom')
@@ -62,13 +63,12 @@ class ProvinceVisTdDotChart {
         customSquare.classList.add('square')
         customSquare.setAttribute('width', `${this.widthSq}`) // .width = `${this.widthSq}px`
         customSquare.setAttribute('height', `${this.widthSq}`) // .height = `${this.widthSq}px`
-        coordinates = calcCoordinates()
+        coordinates = calcCoordinates(calcFactors, i, this.value)
         customSquare.setAttribute('x', coordinates.x)
         customSquare.setAttribute('y', coordinates.y)
         customSquare.setAttribute('fillStyle', this.color)
       }
       height = coordinates.nBlockY + this.widthSq + this.sep * 12 + 10 + 3
-      console.log(customBase.children)
       return customBase
     })
     calcCoordinates.set(this, () => {
