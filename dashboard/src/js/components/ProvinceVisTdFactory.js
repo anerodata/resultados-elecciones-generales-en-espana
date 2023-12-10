d59f28776bfa0ceadee6f2230f0eed0e8f829178
anerodata <antonio92.ahr@gmail.com>
2023-12-10 16:03:07 +0100

    Throw error in factory

--- dashboard/src/js/components/ProvinceVisTdFactory.js
+++ dashboard/src/js/components/ProvinceVisTdFactory.js
@@ -3,29 +3,28 @@ import ProvinceVisTdVariation from './ProvinceVisTdVariation.js'
 import ProvinceVisDotChart from './ProvinceVisTdDotChart.js'
 import tooltipEventSubscriber from './tooltipEventSubscriber.js'
 class ProvinceVisTdFactory {
-  constructor () {
-    this.createTd = (options) => {
-      const { tdType, ...tdProps } = options
-      switch (tdType) {
-        case 'party':
-          return new ProvinceVisTdParty({
-            value: tdProps.value,
-            color: tdProps.color
-          })
-        case 'variation':
-          return new ProvinceVisTdVariation({
-            value: tdProps.value,
-            tooltipEventSubscriber
-          })
-        case 'chart':
-          return new ProvinceVisDotChart({
-            value: tdProps.value,
-            color: tdProps.color,
-            chartDimensions: tdProps.chartDimensions,
-            tooltipEventSubscriber
-          })
-      }
+  createTd (options) {
+    const { tdType, ...tdProps } = options
+    switch (tdType) {
+      case 'party':
+        return new ProvinceVisTdParty({
+          value: tdProps.value,
+          color: tdProps.color
+        })
+      case 'variation':
+        return new ProvinceVisTdVariation({
+          value: tdProps.value,
+          tooltipEventSubscriber
+        })
+      case 'chart':
+        return new ProvinceVisDotChart({
+          value: tdProps.value,
+          color: tdProps.color,
+          chartDimensions: tdProps.chartDimensions,
+          tooltipEventSubscriber
+        })
     }
+    throw new Error(`Unknown tdType: ${tdType}`)
   }
 }
 export default ProvinceVisTdFactory
