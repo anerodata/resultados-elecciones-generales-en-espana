0fe1bbcb5b8942bab8d08deaffff65d7c58f9968
anerodata <antonio92.ahr@gmail.com>
2023-12-13 23:24:16 +0100

    Sort parliament data

--- src/js/fetch/ParliamentCSVFetcher.js
+++ src/js/fetch/ParliamentCSVFetcher.js
@@ -1,22 +1,61 @@
 import Papa from 'papaparse'
 import getParliamentVotesData from './getParliamentVotesData.js'
+
 const parseCSVtoJSON = new WeakMap()
+const getVotesAndDeputiesData = new WeakMap()
+const sortParliamentData = new WeakMap()
+const strVotesToReplace = '_votos'
+const strDeputiesToReplace = '_diputados'
+
+const formatVotesKey = (key) => key.replace(strVotesToReplace, '')
+const formatDeputiesKey = (key) => key.replace(strDeputiesToReplace, '')
+
 class ParliamentCSVFetcher {
   constructor (proccessId) {
     this.proccessId = proccessId
 
     parseCSVtoJSON.set(this, (csvString) => {
-      return Papa.parse(csvString, {
+      const parsedCSV = Papa.parse(csvString, {
         header: true,
         skipEmptyLines: true
       })
+      return parsedCSV.data
+    })
+
+    getVotesAndDeputiesData.set(this, (data) => {
+      const votesAndDeputiesData = { votes: [], deputies: [] }
+      data.forEach(row => {
+        const sortedData = sortParliamentData.get(this)(row)
+        votesAndDeputiesData.votes.push(sortedData.votes)
+        votesAndDeputiesData.deputies.push(sortedData.deputies)
+      })
+      return votesAndDeputiesData
+    })
+
+    sortParliamentData.set(this, (row) => {
+      const sortedData = { votes: {}, deputies: {} }
+      for (const key in row) {
+        if (key.includes(strVotesToReplace)) {
+          sortedData.votes[formatVotesKey(key)] = row[key]
+          continue
+        }
+        if (key.includes(strDeputiesToReplace)) {
+          sortedData.deputies[formatDeputiesKey(key)] = row[key]
+          continue
+        }
+        sortedData.votes[key] = row[key]
+        sortedData.deputies[key] = row[key]
+      }
+      return sortedData
     })
   }
 
   async getParliamentJSON () {
     try {
       const parliamentDataCSV = await getParliamentVotesData(this.proccessId)
-      return parseCSVtoJSON.get(this)(parliamentDataCSV)
+      const parliamentDataJSON = parseCSVtoJSON.get(this)(parliamentDataCSV)
+      const votesAndDeputiesData = getVotesAndDeputiesData.get(this)(parliamentDataJSON)
+      return votesAndDeputiesData
     } catch (err) {
       throw new Error(err.message)
     }
