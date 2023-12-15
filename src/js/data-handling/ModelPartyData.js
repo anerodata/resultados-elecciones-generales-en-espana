b27cadab428cd53fa47b5c644dff31c6bcf1ddbd
anerodata <antonio92.ahr@gmail.com>
2023-12-15 20:05:50 +0100

    Create new ModelParty

---
+++ src/js/data-handling/ModelPartyData.js
@@ -0,0 +1,10 @@
+class ModelPartyData {
+  constructor ({ partyName, votesNum, votesPreviousNum, color, diff }) {
+    this.nombre = partyName
+    this.votesNum = votesNum
+    this.votesPreviousNum = votesPreviousNum
+    this.color = color
+    this.diff = diff
+  }
+}
+export default ModelPartyData
