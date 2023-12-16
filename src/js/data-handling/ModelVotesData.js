d626e42dea803c580f67355ae68109f3bbb84537
anerodata <antonio92.ahr@gmail.com>
2023-12-16 20:45:20 +0100

    Store votes per dot in constant

--- src/js/data-handling/ModelVotesData.js
+++ src/js/data-handling/ModelVotesData.js
@@ -1,10 +1,11 @@
+const votesPerDot = 100
 class ModelVotesData {
   constructor ({ partyName, initials, defaultName, votesNum, votesPreviousNum, color, diff }) {
     this.nombre = partyName
     this.initials = initials || partyName
     this.defaultName = defaultName || partyName
-    this.votesNum = votesNum ? Number(votesNum / 100) : undefined
-    this.votesPreviousNum = votesPreviousNum ? Number(votesPreviousNum / 100) : undefined
+    this.votesNum = votesNum ? Number(votesNum / votesPerDot) : undefined
+    this.votesPreviousNum = votesPreviousNum ? Number(votesPreviousNum / votesPerDot) : undefined
     this.color = color || 'grey'
     this.diff = Number(diff) || 0
   }
