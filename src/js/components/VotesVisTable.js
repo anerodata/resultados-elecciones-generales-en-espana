1a870693b09f559ebbf146863fe42e2e8caf69e9
anerodata <antonio92.ahr@gmail.com>
2023-12-29 23:33:44 +0100

    Build no chart td first

--- src/js/components/VotesVisTable.js
+++ src/js/components/VotesVisTable.js
@@ -7,6 +7,8 @@ const getTableHead = new WeakMap()
 const getTableBody = new WeakMap()
 const getTableTr = new WeakMap()
 const getTableTd = new WeakMap()
+const feedTableTd = new WeakMap()
+const feedNoChartTd = new WeakMap()
 const provinceVisTdFactory = new VotesVisTdFactory()
 
 class VotesVisTable {
@@ -47,32 +49,48 @@ class VotesVisTable {
 
     getTableBody.set(this, () => {
       const tBody = document.createElement('tbody')
-      dataset.forEach(row => {
-        const tR = getTableTr.get(this)(row)
+      dataset.forEach(() => {
+        const tR = getTableTr.get(this)()
         tBody.appendChild(tR)
       })
       return tBody
     })
 
-    getTableTr.set(this, (row) => {
+    getTableTr.set(this, () => {
       const tR = document.createElement('tr')
       headData.forEach(headField => {
-        const tD = getTableTd.get(this)(row, headField)
+        const tD = getTableTd.get(this)(headField)
         tR.appendChild(tD)
       })
       return tR
     })
 
-    getTableTd.set(this, (row, headField) => {
+    getTableTd.set(this, (headField) => {
       const tD = document.createElement('td')
+      tD.classList.add(`vis-table_cell--${headField.type}`)
+      tD.setAttribute('data-cell-type', headField.type)
+      return tD
+    })
+
+    feedNoChartTd.set(this, (trs) => {
+      trs.forEach((tr, i) => {
+        const tdsNotVis = tr.querySelectorAll('td:not(.vis-table_cell--chart)')
+        const headNoChart = this.headData.filter(headField => headField.type !== 'chart')
+        tdsNotVis.forEach((td, j) => {
+          const tdContent = feedTableTd.get(this)(this.dataset[i], headNoChart[j])
+          td.appendChild(tdContent)
+        })
+      })
+    })
+
+    feedTableTd.set(this, (row, headField) => {
       const tDContent = provinceVisTdFactory.createTd({
         tdType: headField.type,
         value: row[headField.name],
         color: row.color,
         chartDimensions: this.chartDimensions
       })
-      tD.appendChild(tDContent.getTdNode())
-      return tD
+      return tDContent.getTdNode()
     })
   }
 
@@ -81,6 +99,8 @@ class VotesVisTable {
     const table = document.createElement('table')
     const tHead = getTableHead.get(this)()
     const tableBody = getTableBody.get(this)()
+    const trs = tableBody.querySelectorAll('tr')
+    feedNoChartTd.get(this)(trs)
     table.appendChild(tHead)
     table.appendChild(tableBody)
     this.tableContainer.textContent = ''
