e58029bd07f3892d115268d721dbd519b3c17436
anerodata <antonio92.ahr@gmail.com>
2024-01-27 20:26:02 +0100

    Set tooltip top position

--- src/js/components/Tooltip.js
+++ src/js/components/Tooltip.js
@@ -1,21 +1,22 @@
 class Tooltip {
   constructor (id) {
     this.tooltipContainer = document.getElementById(id)
-    this.padBetCursorAndTooltip = 15
+    this.padBetCursorAndTooltip = 10
   }
 
   setTooltipXPositionLeft (x) {
-    this.tooltipContainer.style.right = window.innerWidth - x + this.padBetCursorAndTooltip + 'px'
+    this.tooltipContainer.style.right = `${window.innerWidth - x + this.padBetCursorAndTooltip}px`
     this.tooltipContainer.style.left = ''
   }
 
   setTooltipXPositionRight (x) {
-    this.tooltipContainer.style.left = x + this.padBetCursorAndTooltip + 'px'
+    this.tooltipContainer.style.left = `${x + this.padBetCursorAndTooltip}px`
     this.tooltipContainer.style.right = ''
   }
 
   setTooltipYPosition (y) {
-    this.tooltipContainer.style.top = y - 30 + 'px'
+    const tooltipHeight = this.tooltipContainer.offsetHeight
+    this.tooltipContainer.style.top = `${y - tooltipHeight + this.padBetCursorAndTooltip}px`
   }
 
   setTooltipContent (htmlContent) {
